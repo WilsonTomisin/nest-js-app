2fa37d81879b22f0770f1e407adcafb010093187
Wilson Tomisin <tommywilson972@gmail.com>
2025-02-14 10:23:23 +0100

    removed folder

--- client/src/pages/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Homepage} from './Homepage';
-export {default as Login} from './Login';
-export {default as Signup} from './Signup';
-export {default as MyNotes} from './MyNotes'
--- client/src/utils/getServer.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const getServer = axios.create({
-    baseURL:"https://nest-js-app-5gjg.onrender.com/api"
-})
